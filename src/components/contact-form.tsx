83f27b2d362595804a66153b29cce382e2cc528b
Jeroen van Vlaanderen <jeroenvanvlaanderen@gmail.com>
2025-10-08 17:10:18 +0000

    Remove the linkedin profile in the contact page

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -52,14 +52,6 @@ const contactMethods = [
         contact: "hello@example.com" // Replace with actual email
     },
     {
-        icon: (
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
-        ),
-        isLink: true,
-        contact: "https://www.linkedin.com/in/paula-rodriguez-rebollar-67187b373/",
-        text: "LinkedIn Profile"
-    },
-    {
         icon:
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
