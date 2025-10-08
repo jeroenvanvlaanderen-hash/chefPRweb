f85e9867c3876bb879c94c21fa8e2d67f847f7f8
Jeroen van Vlaanderen <jeroenvanvlaanderen@gmail.com>
2025-10-08 17:16:14 +0000

    I have changed my formspree account. The contact form should be linked t

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -84,7 +84,7 @@ export function ContactForm() {
         setIsSubmitting(true);
         
         try {
-          const response = await fetch("https://formspree.io/f/mjkardje", {
+          const response = await fetch("https://formspree.io/f/xwprnaqz", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
